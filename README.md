1ae2182f69e5c7d17723a75f1115529986f7b168
arthurassists <apetbrz@gmail.com>
2024-06-10 14:04:25 -0400

    implemented adding and subtracting binary numbers

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ for binary -> decimal: prefix binary number with 'b' (i.e. b0100 -> d4)
 
 for decimal -> binary: prefix binary number with 'd' (i.e. d5 -> b0101)
 
+supports binary math: prefix expression with 'b', don't use spaces (i.e. b1111+1001 -> b11000)
+
 to exit: 'e'
--- binary-dec-tool.py
+++ binary-dec-tool.py
@@ -1,17 +1,42 @@
 def b2d(input):
     return "d" + format(int(input[1:], 2))
+
 def d2b(input):
     return ("" + '{0:#b}'.format(int(input[1:])))[1:]
+
+def badd(input):
+    index = var.find('+')
+    return ("" + '{0:#b}'.format(int(input[1:index], 2) + int(input[(index+1):], 2)))[1:]
+
+def bsub(input):
+    index = var.find('-')
+    return ("" + '{0:#b}'.format(int(input[1:index], 2) - int(input[(index+1):], 2)))[1:]
 
 i = 1
 while i == 1:
     var = input(":")
+
     if(var == ""): continue
+
     if(var[0] == "b"):
+
+        if(var.find('+')):
+            try: print(badd(var))
+            except: print('err')
+            continue
+        
+        if(var.find('-')):
+            try: print(bsub(var))
+            except: print('err')
+            continue
+
         try: print(b2d(var))
         except: print('err')
+
     if(var[0] == 'd'): 
+
         try: print(d2b(var))
         except: print('err')
+
     if(var[0] == 'e'):
         exit(1)
