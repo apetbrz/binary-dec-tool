369dd8b87e2ed90fd28a38b99769afffb1ff2fec
arthurassists <apetbrz@gmail.com>
2024-06-10 12:24:42 -0400

    init

---
+++ binary-dec-tool.py
@@ -0,0 +1,15 @@
+def b2d(input):
+    return "d" + format(int(input[1:], 2))
+def d2b(input):
+    return ("" + '{0:#b}'.format(int(input[1:])))[1:]
+
+i = 1
+while i == 1:
+    var = input(":")
+    if(var == ""): continue
+    if(var[0] == "b"):
+        try: print(b2d(var))
+        except: print('err')
+    if(var[0] == 'd'): 
+        try: print(d2b(var))
+        except: print('err')
